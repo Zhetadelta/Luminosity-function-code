4d96cb288eee3f70de7046ec86a194ced0dcc9ab
Sakimori <sakimorihalley@gmail.com>
2023-11-13 15:21:01 -0500

    save plots to image, add relevant text to plot

--- LuminosityFunctions.py
+++ LuminosityFunctions.py
@@ -1,7 +1,7 @@
 #  infer number of pulsars in a globular cluster given a luminosity function, number of observed pulsars and minimum luminosity
-import math
-import csv
+import math, csv
 import numpy as np
+from os import path
 from scipy.special import erfc
 import matplotlib.pyplot as plt
 
@@ -65,14 +65,13 @@ for clusterName in clusters.keys(): #loop through every cluster in dataset
         results = [likelihood(n) for n in n_values]
 
         # Plot the probability distribution of N
+        plt.clf() #clear figure
         plt.plot(n_values, results)
-        plt.xlabel(f"Estimate of true number of pulsars in {clusterName}")
+        plt.title(clusterName)
+        plt.xlabel(f"Estimate of true number of pulsars \nBest estimate: {nhat} @ {round(likelihood(nhat)*100,2)}%")
         plt.ylabel('Model likelihood')
-        plt.show()
-
-        # Summarize the assumptions and results of this analysis
-        print (f"In {clusterName}, we observe",o,"pulsars above",l,"mJy kpc^2")
-        print ("Assuming a log-normal LF with mean",a,"and sigma",b)
-        print ("In this case, the fraction of the LF probed is",fraction_above(l))
-        print ("Best estimate for this cluster is",nhat,"pulsars")
-        print ("Maximum likelihood in this case is",likelihood(nhat))
+        plt.subplots_adjust(bottom=0.25)
+        #Add text summarizing assumptions and results
+        plt.gcf().text(0.98,0.03,f"{o} pulsars \nabove {l} mJy kpc^2\n({round(fraction_above(l)*100,2)}% probed)", horizontalalignment='right')
+        plt.gcf().text(0.02,0.05,f"Assuming log-normal of\nmean {a} and sigma {b}")
+        plt.savefig(path.join(".","plots",f"{clusterName}.png"))
